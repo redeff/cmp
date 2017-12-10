af7a506e57a9d0b44aba31e9cb1da175afe5f6da
redeff <14carlosoto@gmail.com>
2017-12-10 15:48:59 -0300

    .

--- oia/train/tanda1/clima/main.cpp
+++ oia/train/tanda1/clima/main.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Guarda un array implicito de 1s y 0s
-vector<int> ft;
-
 int lsb(int x) {
   return x & -x;
 }
 
 // Toma la suma de los valores del
 // array en [0, index)
-int get_ft(int index) {
+int get_ft(int index, vector<int> &ft) {
   int v = 0;
   for(int x = index; x > 0; x -= lsb(x)) {
     v += ft[x];
@@ -19,7 +16,7 @@ int get_ft(int index) {
 }
 
 // Aumenta en 1 el valor en indice $index
-void set_ft(int index) {
+void set_ft(int index, vector<int> &ft) {
   index += 1;
   for(int x = index; x < ft.size(); x += lsb(x)) {
     ft[x] += 1;
@@ -29,29 +26,9 @@ void set_ft(int index) {
 #define ins cin
 #define ous cout
 
-struct Estacion {
-  int p;
-  int q;
-  int index;
-
-  Estacion(int p_, int q_, int index_):
-    p(p_), q(q_), index(index_) {}
-};
-
-struct Estaciones {
-  int p;
-  int q;
-  vector<int> is;
-
-  int index;
-
-  Estaciones(int p_, int q_, vector<int> is_, int index_):
-    p(p_), q(q_), is(is_), index(index_) {}
-};
-
 template<typename T, typename FuncA, typename FuncB>
-vector<pair<T, int>> comp(vector<T> ns, FuncA comp1, FuncB comp2) { 
-  ft = vector<int>(ns.size() + 1, 0);
+vector<pair<T, int>> count_greater(vector<T> ns, FuncA comp1, FuncB comp2) { 
+  vector<int> ft = vector<int>(ns.size() + 1, 0);
   vector<pair<T, int>> out;
   sort(ns.begin(), ns.end(), comp1);
   stable_sort(ns.begin(), ns.end(), comp2);
@@ -61,123 +38,60 @@ vector<pair<T, int>> comp(vector<T> ns, FuncA comp1, FuncB comp2) {
     with_index.emplace_back(ns[i], i);
   }
 
-  stable_sort(with_index.begin(), with_index.end(), [&](pair<T, int> t1, pair<T, int> t2) {
-    return comp1(t1.first, t2.first);
-  });
+  stable_sort(
+      with_index.begin(), 
+      with_index.end(), 
+      [&](pair<T, int> t1, pair<T, int> t2) {
+        return comp1(t1.first, t2.first);
+      }
+  );
 
   for(int i = 0; i < with_index.size(); ++i) {
-    set_ft(with_index[i].second);
-    out.emplace_back(with_index[i].first, get_ft(with_index[i].second));
+    set_ft(with_index[i].second, ft);
+    out.emplace_back(with_index[i].first, get_ft(with_index[i].second, ft));
   }
   return out;
 }
 
-int main() {
-  int n;
-  ins >> n; // numero de estaciones
-  /*vector<pair<int, int>> pairs = {{1, 100}, {3, 4}, {5, 6}};
-  auto result = comp(
-    pairs, 
-    [](pair<int, int> p1, pair<int, int> p2) {
-      return p1.first < p2.first;
-    },
-    [](pair<int, int> p1, pair<int, int> p2) {
-      return p1.second < p2.second;
-    }
-  );
-  for(auto r : result) {
-    cout << r << endl;
-  }*/
-
-  vector<Estacion> es;
-
-  for(int i = 0; i < n; ++i) {
-    int p = 33333333; // Minima
-    int q = -33333333; // Maxima
-    int r;
-    cin >> r;
-    for(int j = 0; j < r; ++j) {
-      int pl, ql;
-      cin >> pl >> ql;
-      p = min(p, pl);
-      q = min(q, ql);
-    }
-
-    es.emplace_back(-p, q, i + 1);
-  }
-
-
-  stable_sort(es.begin(), es.end(), [](Estacion a, Estacion b) {
-    return a.p < b.p;
-  });
-
-  stable_sort(es.begin(), es.end(), [](Estacion a, Estacion b) {
-    return a.q < b.q;
-  });
-
-  vector<Estaciones> ess;
-  int index = 0;
-  for(int i = 0; i < n; ++i) {
-    if(ess.empty() || ess.back().p != es[i].p || ess.back().q != es[i].q) {
-      ess.push_back(Estaciones(es[i].p, es[i].q, {es[i].index}, index));
-      index++;
-    } else {
-      ess.back().is.push_back(es[i].index);
-    }
-  }
-
-  vector<pair<int, int>> extremes(ess.size());
-  for(auto p : comp(
-        ess,
-        [](Estaciones p1, Estaciones p2) {
-          return p1.p < p2.p;
-        },
-        [](Estaciones p1, Estaciones p2) {
-          return p1.q < p2.q;
-        }
-  )) {
-      extremes[p.first.index].first = p.second;
-  }
-  for(auto p : comp(
-        ess,
-        [](Estaciones p1, Estaciones p2) {
-          return p1.p > p2.p;
-        },
-        [](Estaciones p1, Estaciones p2) {
-          return p1.q > p2.q;
-        }
-  )) {
-      extremes[p.first.index].second = p.second;
-  }
-
-  vector<pair<int, int>> total(n);
-  for(int i = 0; i < extremes.size(); ++i) {
-    total[extremes[i].
-
-  /*stable_sort(es.begin(), es.end(), [](Estacion a, Estacion b) {
-    return a.p < b.p;
-  });
-
-  vector<int> mas_extremas;
-  ft = vector<int>(ess.size() + 1, 0);
-
-  for(int i = 0; i < ess.size(); ++i) {
-    set_ft(ess[i].index);
-    mas_extremas.push_back(get_ft(ess[i].index));
-  }
-
-  vector<int> maximums;
-  for(int i = 0; i < mas_extremas.size(); ++i) {
-    if(maximums.empty() || mas_extremas[i] > mas_extremas[maximums.back()]) {
-      maximums = {i};
-    } else if (mas_extremas[i] == mas_extremas[maximums.back()]) {
-      maximums.push_back(i);
-    }
-  }
-  for(int m : maximums) {
-    cout << "tata" << endl;
-    for(int i : ess[m].is) {
-      cout << i << " " << mas_extremas[m] << endl;
-    }
-  }*/
+struct Station {
+  int mini;
+  int maxi;
+  Station(int mini, int maxi)
+    : mini(mini)
+    , maxi(maxi)
+  {}
 }
+
+vector<int> inv_per(vector<int> &ns) {
+  vector<int> ps(ns.size());
+  for(int i = 0; i < ns.size(); ++i) {
+    ps[ns[i]] = i;
+  }
+
+  return ps;
+}
+
+
+// Given a list of Ts such that no two of them are equal
+// under both comp_a and comp_b, calculate the ones that
+// are less than most elements, return their indices, and
+// the amount of other Ts they're incomparable with
+template<typename T, typename FuncA comp_a, typename FuncB>
+vector<pair<int, int>> from_distinct(
+    vector<T> ns, 
+    FuncA comp_a, 
+    FuncB comp_b) {
+  vector<int> indices(ns.size());
+  iota(indices.begin(), indices.end(), 0);
+  sort(
+      indices.begin(), 
+      indices.end(), 
+      [&](int i1, int i2) {
+        return comp_a(ns[i1], ns[i2]);
+      }
+  );
+}
+
+int main() {
+}
+
