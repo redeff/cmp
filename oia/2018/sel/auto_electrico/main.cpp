283640b3fa36147afa4c6bcc99df62001247a935
redeff <14carlosoto@gmail.com>
2018-05-31 20:12:40 -0300

    electronmovil 2018 done

--- oia/2018/sel/auto_electrico/main.cpp
+++ oia/2018/sel/auto_electrico/main.cpp
@@ -1,10 +1,30 @@
 #include <vector>
 
 using namespace std;
+const int INF = 333333333;
 
-vector<int> electromovil(int E, vector<int> ubicacion, vector<int> autonomia)
-{
-    vector<int> ret;
-    ret.push_back(27);
-    return ret;
+vector<int> electromovil(int n, vector<int> start, vector<int> len) {
+    len[n - 1] = INF;
+    vector<int> end(n);
+    for(int i = 0; i < n; ++i) {
+        end[i] = start[i] + len[i];
+    }
+
+    vector<int> pat;
+    pat.push_back(0);
+    while(true) {
+        int best = pat.back();
+        int curr = pat.back();
+        for(int i = curr; i < n && start[i] <= end[curr]; ++i) {
+            if(end[i] > end[best]) best = i;
+        }
+        if(best == curr) break;
+        pat.push_back(best);
+    }
+    pat.erase(pat.begin());
+    if(pat.back() != n - 1) return {};
+    else {
+        for(int i = 0; i < pat.size(); ++i) pat[i] = start[pat[i]];
+        return pat;
+    }
 }
